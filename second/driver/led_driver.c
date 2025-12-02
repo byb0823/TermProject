3cc8950a58ef12a9e3f85d140f458bdf7d3ad82f
yeongbinbae <yeongbinbae01@gmail.com>
2025-12-02 11:41:28 +0900

    commit

--- second/driver/led_driver.c
+++ second/driver/led_driver.c
@@ -50,25 +50,32 @@ static void toggle_all_led(void)
 static void set_single_mode(void)
 {
     int i;
-    bool found = false;
+    int current_led = -1;
 
+    /* 현재 켜진 LED 찾기 */
     for (i = 0; i < 4; i++) {
         if (led_state[i] == HIGH) {
-            found = true;
+            current_led = i;
             break;
         }
     }
 
-    if (found) {
-        led_state[i] = LOW;
-        gpio_set_value(led[i], LOW);
-        i = (i + 1) % 4;
-        led_state[i] = HIGH;
-        gpio_set_value(led[i], HIGH);
+    /* 현재 LED 끄기 */
+    if (current_led >= 0) {
+        led_state[current_led] = LOW;
+        gpio_set_value(led[current_led], LOW);
+        
+        /* 다음 LED 켜기 */
+        current_led = (current_led + 1) % 4;
     } else {
-        led_state[0] = HIGH;
-        gpio_set_value(led[0], HIGH);
+        /* 켜진 LED가 없으면 첫 번째부터 */
+        current_led = 0;
     }
+    
+    led_state[current_led] = HIGH;
+    gpio_set_value(led[current_led], HIGH);
+    
+    printk(KERN_INFO "Single mode: LED[%d] ON\n", current_led);
 }
 
 static void toggle_manual_led(int sw_index)
@@ -124,6 +131,8 @@ static long led_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
     case MODE_SINGLE:
         del_timer(&led_timer);
         set_all_led(LOW);
+        led_state[0] = HIGH;
+        gpio_set_value(led[0], HIGH);
         current_mode = MODE_SINGLE;
         mod_timer(&led_timer, jiffies + HZ * 2);
         printk(KERN_INFO "SW1 pressed: MODE_SINGLE ON!\n");
